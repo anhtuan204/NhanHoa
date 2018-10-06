0d0b351c4ff2de6535aeacf853914186166cd46b
Đỗ Xuân Sơn <do.xuanson181295@gmail.com>
2018-10-06 10:02:39 +0700

    Update "Nexus"

--- Nexus/scripts/nexus-install-on-centos7.5.1804.sh
+++ Nexus/scripts/nexus-install-on-centos7.5.1804.sh
@@ -23,6 +23,7 @@ setenforce 0
 # Install Java Runtime Environment
 echo "###Install Java Runtime Environment###"
 sleep 3
+cd /root
 wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u181-b13/96a7b8442fe848ef90c96a2fad6ed6d1/jdk-8u181-linux-x64.rpm
 yum localinstall jdk-8u181-linux-x64.rpm -y
 
